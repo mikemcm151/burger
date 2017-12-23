6fcc48e3f60e3dc365052b5d89611d1a53927c0d
mikemcm151 <mikemcm151@gmail.comgit config --global user.name mikemcm151git config --global user.email mikemcm151@gmail.com>
2017-12-22 18:17:21 -0600

    mods

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('California Burger');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Burger');
+INSERT INTO burgers (burger_name) VALUES ('Guacamole Burger');
+-- INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
+-- INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
+-- INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
+
